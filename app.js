c33d0a1dc2ef3b52d12b42c0818164c703beb0e1
Alexander BELCHITSKIY <alexander.belchitskiy@1000geeks.com>
2022-05-30 23:23:44 +0300

    refactor: ADVANCED add BEM according to rule 2.1 in index.html,         add BEM according to rule 2.1 in style.css,         add BEM according to rule 2.1 in app.js

--- app.js
+++ app.js
@@ -33,17 +33,17 @@ var createNewTaskElement=function(taskString){
   var deleteButtonImg=document.createElement("img");//delete button image
 
   label.innerText=taskString;
-  label.className='task';
+  label.className='center-element__task';
 
   //Each elements, needs appending
   checkBox.type="checkbox";
   editInput.type="text";
-  editInput.className="task";
+  editInput.className="center-element__task";
 
   editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
-  editButton.className="button edit";
+  editButton.className="center-element__button center-element__button-edit";
 
-  deleteButton.className="button delete";
+  deleteButton.className="center-element__button center-element__button-delete";
   deleteButtonImg.src='./remove.svg';
   deleteButton.appendChild(deleteButtonImg);
 
@@ -84,7 +84,7 @@ var editTask=function(){
 
   var editInput=listItem.querySelector('input[type=text]');
   var label=listItem.querySelector("label");
-  var editBtn=listItem.querySelector(".edit");
+  var editBtn=listItem.querySelector(".center-element__button-edit");
   var containsClass=listItem.classList.contains("editMode");
   //If class of the parent is .editmode
   if(containsClass){
@@ -156,8 +156,8 @@ var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
   console.log("bind list item events");
   //select ListItems children
   var checkBox=taskListItem.querySelector("input[type=checkbox]");
-  var editButton=taskListItem.querySelector("button.edit");
-  var deleteButton=taskListItem.querySelector("button.delete");
+  var editButton=taskListItem.querySelector("button.center-element__button-edit");
+  var deleteButton=taskListItem.querySelector("button.center-element__button-delete");
 
 
   //Bind editTask to edit button.
